d4b4c48f2a4e6b213e07d0eda42d768e9c9af053
eleazar <eleazarc799@gmail.com>
2025-07-08 00:13:37 -0400

    feat(): completando pagina y filtros de Supplier

--- inventario/src/main/java/org/example/inventario/repository/inventory/ProductRepository.java
+++ inventario/src/main/java/org/example/inventario/repository/inventory/ProductRepository.java
@@ -1,10 +1,15 @@
 package org.example.inventario.repository.inventory;
 
 import org.example.inventario.model.entity.inventory.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+    Page<Product> findAll(Specification<Product> spec, Pageable pageable);
 }
